f5a8941cef5a5d352fc32478a653a684fa7341c1
Leticia Rosae <leticiarosadeoliveira@yahoo.com.br>
2023-05-14 22:34:33 -0300

    Finalized the design

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import styles from "./App.module.css";
 import logo from "./assets/Logo.png";
 import "./global.css";
+import { PlusCircle } from "@phosphor-icons/react";
 
 export function App() {
   return (
-    <>
+    <main>
       <header className={styles.header}>
         <img className={styles.logo} src={logo} alt="" />
       </header>
 
-      <div className={styles.container}>
+      <div className={styles.containerTasks}>
         <form className={styles.tasks}>
           <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button type="submit">Criar</button>
+          <button type="submit">
+            Criar
+            <PlusCircle size={20} />{" "}
+          </button>
         </form>
 
         <div className={styles.info}>
-          <div>
+          <div className={styles.infoTarefas}>
             <p>Tarefas Criadas</p>
             <span>0</span>
           </div>
-          <div>
+          <div className={styles.infoTarefas}>
             <p>Tarefas Concluidas</p>
             <span>0</span>
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
